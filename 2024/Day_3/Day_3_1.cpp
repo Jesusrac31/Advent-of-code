d39ccf7a363fbe46ed8498abf259337c8d1f7e5e
Jesusrac31 <jessuraceros@gmail.com>
2024-12-03 08:57:06 +0100

    03-12-2024-08:57

---
+++ 2024/Day_3/Day_3_1.cpp
@@ -0,0 +1,216 @@
+//Librerías incluidas en #include<bitstdc++.h>
+#include<algorithm>
+#include<array>
+#include<atomic>
+#include<bitset>
+#include<ccomplex>
+#include<cerrno>
+#include<cfenv>
+#include<cfloat>
+#include<chrono>
+#include<cinttypes>
+#include<ciso646>
+#include<climits>
+#include<clocale>
+#include<cmath>
+#include<complex>
+#include<condition_variable>
+#include<csetjmp>
+#include<csignal>
+#include<cstdalign>
+#include<cstdarg>
+#include<cstdbool>
+#include<cstddef>
+#include<cstdint>
+#include<cstdio>
+#include<cstdlib>
+#include<cstring>
+#include<ctgmath>
+#include<ctime>
+#include<cwchar>
+#include<cwctype>
+#include<deque>
+#include<exception>
+#include<forward_list>
+#include<fstream>
+#include<functional>
+#include<future>
+#include<initializer_list>
+#include<iomanip>
+#include<ios>
+#include<iosfwd>
+#include<iostream>
+#include<istream>
+#include<iterator>
+#include<limits>
+#include<list>
+#include<locale>
+#include<map>
+#include<memory>
+#include<mutex>
+#include<new>
+#include<numeric>
+#include<ostream>
+#include<queue>
+#include<random>
+#include<ratio>
+#include<regex>
+#include<scoped_allocator>
+#include<set>
+#include<sstream>
+#include<stack>
+#include<stdexcept>
+#include<streambuf>
+#include<string>
+#include<system_error>
+#include<thread>
+#include<tuple>
+#include<type_traits>
+#include<typeindex>
+#include<typeinfo>
+#include<unordered_map>
+#include<unordered_set>
+#include<utility>
+#include<valarray>
+#include<vector>
+#include <stdlib.h>
+using namespace std;
+
+typedef vector<int> vi;
+typedef vector<long long int> vll;
+typedef long long int lli;
+typedef pair<int, int> pii;
+typedef map<string, int> msi;
+typedef map<int, vector<int>> miv;
+
+// Funciones vector
+#define PB(a) push_back(a);
+
+bool sort_func(int a, int b) {
+  if (a < b) {
+    return true;
+  } else {
+    return false;
+  }
+}
+#define ord(vect) sort(vect.begin(), vect.end(), sort_func);
+
+#define borra_el(vect, el) vect.erase(vect.find(el));
+#define borra_range(vect, a, b) vect.erase(a, b);
+#define borra(vect, n) vect.erase(vect.begin() + n);
+#define B begin();
+#define E end();
+#define copia(v1, v2)                                                          \
+  ;                                                                            \
+  copy(v1.begin(), v1.end(), back_inserter(v2));
+
+// Funciones map
+#define F first;
+#define S second;
+
+// Logaritmo de 2
+double log_2 = log(2);
+double log2(int a) { return (log(a) / log_2); }
+
+void Imprime(vi vect) {
+  for (int i = 0; i < vect.size(); i++) {
+    cout << vect[i] << " ";
+  }
+  cout << "\n";
+}
+
+void Imprime2d(vector<vi> vect) {
+  for (int j = 0; j<vect.size(); j++){
+    for (int i = 0; i < vect[j].size(); i++) {
+        cout << vect[j][i] << " ";
+    }
+    cout << "\n";
+  }
+}
+
+void Imprime_set(set<int> s) {
+  copy(s.begin(), s.end(), ostream_iterator<int>(cout, " "));
+  cout << endl;
+}
+
+int maximo_comun_divisor(int a, int b) {
+  int temporal; // Para no perder b
+  while (b != 0) {
+    temporal = b;
+    b = a % b;
+    a = temporal;
+  }
+  return a;
+}
+
+int minimo_comun_multiplo(int a, int b) {
+  return (a * b) / maximo_comun_divisor(a, b);
+}
+
+bool isNumeric(string const &str) {
+  auto it = str.begin();
+  while (it != str.end() && isdigit(*it)) {
+    it++;
+  }
+  return !str.empty() && it == str.end();
+}
+
+vi lee(int n) {
+  int el;
+  vi vect;
+  for (int i = 0; i < n; i++) {
+    cin >> el;
+    vect.PB(el);
+  }
+  return (vect);
+}
+
+int solve() {
+  // Code aquí
+  vector<string> data;
+  string el;
+  while (cin >> el){
+    data.PB(el);
+  }
+  vector<pii> pos;
+  pii par_el;
+  int tot = 0;
+  for (int i = 0; i<data.size(); i++){
+    for (int j = 3; j<data[i].size(); j++){
+        if (data[i].substr(j-3, 4) == "mul("){
+            bool coma = false;
+            int pos_coma;
+            for (int k = j+1; k<data[i].size(); k++){
+                if (data[i][k] == ')'){
+                    if (coma){
+                        int num1, num2;
+                        num1 = stoi(data[i].substr(j+1, pos_coma-j-1));
+                        num2 = stoi(data[i].substr(pos_coma+1, k-pos_coma-1));
+                        cout << num1 << " " << num2 << endl;
+                        tot += num1*num2;
+                    }
+                    break;
+                }
+                if (!(isNumeric(data[i].substr(k,1)) || (data[i][k] == ',' && (!coma)))){
+                    break;
+                }
+                if (data[i][k] == ','){
+                    coma = true;
+                    pos_coma = k;
+                }
+            }
+        }
+    }
+  }
+  cout << tot << endl;
+  
+  return 0;
+}
+
+int main() {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+  cout.tie(nullptr); 
+  solve();
+  return 0;
+}
